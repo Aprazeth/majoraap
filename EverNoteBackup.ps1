d65dfb88169c574947fddc8c4555e02f443b2a80
MvdG <aprazeth@gmail.com>
2015-06-07 11:00:23 +0200

    Add .LINK for Help
    1. Added .LINK section for supporting "Help EverNoteBackup.ps1 -Online"
    2. Fixed formatting in .SYNOPSIS
    3. Removed $ in .PARAMETER
    4. Updated version to 0.2

--- EverNoteBackup.ps1
+++ EverNoteBackup.ps1
@@ -1,13 +1,14 @@
 ﻿<#
 .SYNOPSIS
-	This PowerShell Script can be scheduled (or run manually) to sync your
+    This PowerShell Script can be scheduled (or run manually) to sync your
     EverNote local database, and then export a backup locally. Lastly, it then
     starts your Cloud storage software to store those files in the cloud
 
 .DESCRIPTION
     Created by: Mochtar van de Griendt
-    Version: 0.1
+    Version: 0.2
     Date: 17 May 2015
+    Modified: 7 June 2015
 
 	The script was created based of a CMD file I wrote some time ago, that
     would allow me to have a local EverNote backup. After searching the
@@ -23,24 +24,24 @@
 
     Please note that currently it doesn't have any error handling (maybe later?)
 
-.PARAMETER  $evernotepath
+.PARAMETER  evernotepath
 	This variable needs to be set to where your EverNote installation is. By
     default it will be in C:\Program Files (x86)\Evernote\Evernote\ so you 
     most likely won't need to change this. Observe that the ENscript.exe 
     must be at the end!
 
-.PARAMETER  $cloudstorageprovider
+.PARAMETER  cloudstorageprovider
     This variable needs to be configured to whereever your cloud storage
     provider software is installed. Whether this is DropBox, Box, OneDrive
     OwnCloud, SpiderOak, etc. - be sure to include the executable's name of 
     the software!
 
-.PARAMETER  $backuppath
+.PARAMETER  backuppath
     This variable needs to be configured to where you want your files to be
     stored. It is absolutely VITAL that you use a folder that you have defined
     in your cloud storage software to be backed up!
 
-.PARAMETER  $runtime
+.PARAMETER  runtime
     This variable gets the date and time when the script is run, which is then
     used to append to the file-names. That makes it easy to see which files were
     created when.
@@ -53,12 +54,11 @@
 
     For more formatting options, please run help Get-Date in a PowerShell window.
 
-.PARAMETER  $logfile
+.PARAMETER  logfile
     This variable takes the output from the ENScript.exe and stores it into a 
     logfile. The variable is built using $runtime.
 
-
-.PARAMETER  $evernotebackup
+.PARAMETER  evernotebackup
     This variable is used as the location and name of the exported EverNote
     database. As it is a folder-path and file-name, the usual restrictions apply
     in terms of naming conventions for file-systems.
@@ -75,7 +75,6 @@
     tell it to export the local database to the location as specified in the
     variable $evernotebackup.
 
-
 .EXAMPLE
 	This script can be used with the Task scheduler in Windows, to run as a 
     scheduled task OR you can run it manually whenever you want or need it.
@@ -88,6 +87,10 @@
     So an example would be:
 
         PowerShell -file "C:\OwnCloud\EverNote\EverNoteBackup.ps1"
+
+.LINK
+    https://github.com/Aprazeth/majoraap
+
 #>
 
 
