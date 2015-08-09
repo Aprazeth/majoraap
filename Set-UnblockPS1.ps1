af2cd2d8f087ec03b50dd5a5863f4170a0d117f2
MvdG <aprazeth@gmail.com>
2015-08-09 09:19:47 +0200

    Initial release
    Initial release of the recursive unblock-cmdlet for downloaded
    PowerShell files.

---
+++ Set-UnblockPS1.ps1
@@ -0,0 +1,14 @@
+﻿<#
+.Synopsis
+   Recursively trawls under current path for all PowerShell scripts and unblocks them.
+.DESCRIPTION
+   Recursively trawls under current path for all PowerShell scripts and unblocks them.
+.EXAMPLE
+    .\Set-Unblock.ps1
+.NOTES
+    Nothing too fancy, nothing too complex.
+.LINK
+    https://github.com/Aprazeth/majoraap/
+#>
+(Get-ChildItem -Filter *.ps1 -Path ("$pwd.path") -Recurse ) | Unblock-File
+Write-Output "Done, all files should be unblocked now."
