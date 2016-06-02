161fbe091462554933924f79aa4888b43032300c
Aprazeth <aprazeth@gmail.com>
2016-06-02 08:55:58 +0200

    Setting default values in PARAM block of Backup-EverNoteDatabase.ps1
    - Set default values for parameters in PARAM block.
    - Replaced "C:\Program Files (x86)" with ${env:ProgramFiles(x86)} in
    default values.
    - Added check to confirm EverNotePath contains "ENScript.exe"; if not to
    add it.
    - Added .INPUTS and .OUTPUTS.
    - Added output to record start-time/date of script (for
    troubleshooting/debugging).
    - Started adding Write-Debug output in script.
    - Added text explaining the script is being aborted if that is the case
    (ie. when it can't start or find EverNote, the backup, or the cloud
    storage provider)

--- Backup-EverNoteDatabase.ps1
+++ Backup-EverNoteDatabase.ps1
@@ -15,22 +15,26 @@
     The script sets several variables, that you can alter via parameters, such as 
     the install-path of EverNote, the cloud software you want to use, 
     as well as the location of exported EverNote database.
-
-    Please note that currently it doesn't have any error handling (maybe later?)
 .PARAMETER EverNotePath
 	This variable needs to be set to where your EverNote installation is. By
     default it will be in C:\Program Files (x86)\Evernote\Evernote\ so you 
-    most likely won't need to change this. Observe that the ENscript.exe 
+    most likely won't need to provide this. Observe that the ENscript.exe 
     must be at the end!
+
+    A default value is provided if none is specified.
 .PARAMETER CloudStorageProvider
     This variable needs to be configured to whereever your cloud storage
     provider software is installed. Whether this is DropBox, Box, OneDrive
     OwnCloud, SpiderOak, etc. - be sure to include the executable's name of 
     the software!
+
+    A default value is provided if none is specified.
 .PARAMETER BackupPath
-    This variable needs to be configured to where you want your files to be
+    This variable needs to be provided to where you want your files to be
     stored. It is absolutely VITAL that you use a folder that you have defined
     in your cloud storage software to be backed up!
+
+    A default value is provided if none is specified.
 .EXAMPLE
 	This script can be used with the Task scheduler in Windows, to run as a 
     scheduled task OR you can run it manually whenever you want or need it.
@@ -43,9 +47,16 @@
     So an example would be:
 
         PowerShell -file "C:\OwnCloud\EverNote\Backup-EverNoteDatabase.ps1"
-
 .LINK
     https://github.com/Aprazeth/majoraap
+.INPUTS
+    [System.String]
+        Input provided by parameters (if specified)
+.OUTPUTS
+    [System.String]
+        Output provided via console; 
+        (file) Extracted EverNote database in BackupPath
+        (file) logfile(s) in BackupPath
 #>
 #requires -version 2.0
 [CmdletBinding()]
@@ -53,53 +64,55 @@ PARAM(
     [Parameter(Mandatory=$False,
                 ValueFromPipelineByPropertyName=$True,
                 Position=0)]
-    [String]$EverNotePath,
+    [String]$EverNotePath = "${env:ProgramFiles(x86)}\Evernote\Evernote\ENScript.exe",
 
     [Parameter(Mandatory=$False,
                 ValueFromPipelineByPropertyName=$True,
                 Position=1)]
-    [String]$CloudStorageProvider,
+    [String]$CloudStorageProvider = "${env:ProgramFiles(x86)}\ownCloud\owncloud.exe",
     
     [Parameter(Mandatory=$False,
                 ValueFromPipelineByPropertyName=$True,
                 Position=2)]
-    [String]$BackupPath,
-
-    [Parameter(Mandatory=$False,
-                ValueFromPipelineByPropertyName=$True,
-                Position=3)]    
-    [String]$LogFile
+    [String]$BackupPath = "C:\OwnCloud\EverNote\"
     )
-If (-NOT ($EverNotePath))
+Write-Output -InputObject "Script started at $(Get-Date)"
+If (-NOT ($EverNotePath).EndsWith("ENScript.exe"))
     {
-        [String]$EverNotePath = "C:\Program Files (x86)\Evernote\Evernote\ENScript.exe"
-    } # END If NOT $EverNotePath
+        Write-Debug -Message "Provided EverNotePath does not end with ENScript.exe - checking if it is a folder"
+        If (Test-Path -LiteralPath $EverNotePath -PathType Container)
+            {
+                Write-Debug -Message "Confirmed it's a folder"
+                If (($EverNotePath).EndsWith("\"))
+                    {
+                        Write-Debug -Message "EverNotePath ends with \ - appending ENScript.exe"
+                        $EverNotePath = ($EverNotePath) + "ENScript.exe"
+                        Write-Debug -Message "`$EverNotePath value is now $($EverNotePath)"
+                    } # END If $EverNotePath end with \
+                Else
+                    {
+                        Write-Debug -Message "EverNotePath ends with \ - appending ENScript.exe"
+                        $EverNotePath = ($EverNotePath) + "\ENScript.exe"
+                        Write-Debug -Message "`$EverNotePath value is now $($EverNotePath)"
+                    } # END Else $EverNotePath end with \
+            } # END If Test-Path $EverNotePath Container
+    } # END If NOT $EverNotePath endswith ENScript.exe
 If (-NOT (Test-Path -LiteralPath $EverNotePath -PathType Leaf))
     {
-        Write-Warning -Message "Path to EverNote is not valid, please provide the correct path via -EverNotePath."
+        Write-Warning -Message "Path to EverNote is not valid, please provide the correct path via -EverNotePath - Aborting script."
         Break
     } # END If NOT Test-Path $EverNotePath
-If (-NOT ($CloudStorageProvider))
-    {
-        [String]$CloudStorageProvider = "c:\Program Files (x86)\ownCloud\owncloud.exe"
-    } # END If NOT $CloudStorageProvider
 If (-NOT (Test-Path -LiteralPath $CloudStorageProvider -PathType Leaf))
     {
-        Write-Warning -Message "Path to EverNote is not valid, please provide the correct path via -CloudStorageProvider."
+        Write-Warning -Message "Path to EverNote is not valid, please provide the correct path via -CloudStorageProvider - Aborting script."
         Break
     } # END If NOT Test-Path $CloudStorageProvider
-If (-NOT ($BackupPath))
-    {
-        [String]$BackupPath = "C:\OwnCloud\EverNote\"
-    } # END If NOT $BackupPath
 If (-NOT (Test-Path -LiteralPath $BackupPath -PathType Container))
     {
-        Write-Warning -Message "Path to backup-folder is not valid, please provide the correct path via -BackupPath."
+        Write-Warning -Message "Path to backup-folder is not valid, please provide the correct path via -BackupPath - Aborting script."
         Break
     } # END If NOT Test-Path $BackupPath
-# Only change this if you know what you are doing.
 [String]$RunTime = (Get-Date -Uformat "%Y-%m-%d_%H-%M")
-# Stop here - no further changes required.
 [String]$LogFile = $BackupPath + "EverNote" + $RunTime + "_sync.log"
 [String]$EverNoteBackup = $BackupPath + $RunTime + "_bkp_Evernote.enex"
 [String]$EverNoteExport = $BackupPath + "EverNote_" + $RunTime + "_export.log"
@@ -110,7 +123,7 @@ Try
     } # END Try Start-Process $EverNotePath syncdatabase
 Catch
     {
-        Write-Warning -Message "Failed to start Evernote sync - please check if you have the necessary permissions"
+        Write-Warning -Message "Failed to start Evernote sync - please check if you have the necessary permissions - Aborting script."
         Write-Debug -Message "($Error) | Format-List -Force"
         Break
     } # END Catch Start-Process $EverNotePath syncdatabase
@@ -120,7 +133,7 @@ Try
     } # END Try Start-Process $EverNotePath $Arguments
 Catch
     {
-        Write-Warning -Message "Failed to export Evernote database - please check if you have the necessary permissions"
+        Write-Warning -Message "Failed to export Evernote database - please check if you have the necessary permissions - Aborting script."
         Write-Debug -Message "($Error) | Format-List -Force"
         Break
     } # END Catch Start-Process $EverNotePath $Arguments
@@ -130,7 +143,7 @@ Try
     } # END Try Start-Process $CloudStorageProvider
 Catch
     {
-        Write-Warning -Message "Failed to start your CloudStorageProvider - please check if you have the necessary permissions"
+        Write-Warning -Message "Failed to start your CloudStorageProvider - please check if you have the necessary permissions - Aborting script."
         Write-Debug -Message "($Error) | Format-List -Force"
         Break
     } # END Catch Start-Process $CloudStorageProvider
